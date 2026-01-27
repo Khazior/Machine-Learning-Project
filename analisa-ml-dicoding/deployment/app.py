5ad15d0ea6d5f2ef65b32aa4a98c3762e6f49358
Khazior <mekazior9@gmail.com>
2026-01-27 12:51:35 +0800

    customer age redifine

--- analisa-ml-dicoding/deployment/app.py
+++ analisa-ml-dicoding/deployment/app.py
@@ -58,7 +58,7 @@ EXPECTED_COLUMNS = [
     'Location_Tucson', 'Location_Virginia Beach', 'Location_Washington', 
     'Channel_Branch', 'Channel_Online', 
     'CustomerOccupation_Engineer', 'CustomerOccupation_Retired', 'CustomerOccupation_Student', 
-    'CustomerAgeGroup_Muda', 'CustomerAgeGroup_Tua'
+    'CustomerAgeGroup_Young', 'CustomerAgeGroup_Old'
 ]
 
 # ==========================================
@@ -66,9 +66,9 @@ EXPECTED_COLUMNS = [
 # ==========================================
 
 def determine_age_group(age):
-    if age <= 32: return 'Muda'
-    elif age <= 55: return 'Dewasa'
-    else: return 'Tua'
+    if age <= 32: return 'Young'
+    elif age <= 55: return 'Mature'
+    else: return 'Old'
 
 def inverse_transform_data(df, stats, mapping):
     """Converts normalized data back to original format."""
